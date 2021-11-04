bd67be347f046652adcbe3c3005476a689b05dc5
Anna Heselbarth <annaheselbarth@gmail.com>
2021-11-04 19:26:50 -0400

    outing class created

--- ChallengeFourOutingsLibrary/Outing.cs
+++ ChallengeFourOutingsLibrary/Outing.cs
@@ -6,7 +6,42 @@ using System.Threading.Tasks;
 
 namespace ChallengeFourOutingsLibrary
 {
+   
+    
+     public enum EventType { Golf = 1, Bowling, AmusementPark, Concert };
+    
+
     public class Outing
     {
+        public EventType typeOfEvent { get; set; }
+        public int numberOfPeople { get; set; }
+        public DateTime dateOfEvent { get; set; }
+        public decimal costPerPerson { get; set; }
+        public decimal costOfEvent { get; set; }
+
+        public Outing()
+        {
+
+        }
+
+
+        public Outing(EventType typeOfEvent, int numberOfPeople, DateTime dateOfEvent, decimal costPerPerson, decimal costOfEvent)
+        {
+            TypeOfEvent = typeOfEvent;
+            NumberOfPeople = numberOfPeople;
+            DateOfEvent = dateOfEvent;
+            CostPerPerson = costPerPerson;
+            CostOfEvent = costOfEvent;
+
+        }
+
+
+
+
     }
+
+   
+
 }
+
+
--- ChallengeFourOutingsLibrary/OutingRepo.cs
+++ ChallengeFourOutingsLibrary/OutingRepo.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ChallengeFourOutingsLibrary
 {
-    class OutingRepo
+    public class OutingRepo
     {
     }
 }
